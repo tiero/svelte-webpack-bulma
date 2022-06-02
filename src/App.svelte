024a66a43a877722e70768a7b17416600f3b390b
tiero <3596602+tiero@users.noreply.github.com>
2022-06-03 00:07:22 +0200

    Add svelte-marina-button

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,16 @@
+<script type="ts">
+  import Connect, { marinaStore, MarinaStore } from 'svelte-marina-button';
+
+  // You can subscribe to marina status changes
+  marinaStore.subscribe((s: MarinaStore) => {
+    console.log(`Marina is attached to the ${s.network} network`);
+  });
+</script>
+
 <div class="container is-widescreen">
   <div class="box">
-    <h1 class="title">Hello World!</h1>
+    <h1 class="title">Hello fren!</h1>
+    <Connect cssClass={'button'} />
   </div>
 </div>
 
